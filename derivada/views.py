be74c1dd4810a82eb7c0c1e1029b3ff46f53471b
mascDriver <diogoblibi@hotmail.com>
2021-01-26 16:48:36 -0300

    criação de mais opcoes para calculo, grafico e outras coisas

--- derivada/views.py
+++ derivada/views.py
@@ -2,31 +2,82 @@ import base64
 import io
 import urllib
 
-import matplotlib.pyplot as plt
 from django.shortcuts import render
-from sympy import *
+from sympy import Lambda, symbols, init_printing, plot, diff, Limit, Integral, pretty, limit, integrate, sqrt
 
-x = symbols('x')
-xy = symbols('xy')
-y = symbols('y')
-c = symbols('=')
+x, xy, y = symbols('x, xy, y')
+
 
 def home(request):
+    init_printing()
     if request.POST:
-        init_printing()
         data = request.POST.get('expressao')
-        f = Lambda(x, data)
-        derivada = diff(f(x), x)
-        plt.plot(range(2))
-        fig = plt.gcf()
-        buf = io.BytesIO()
-        fig.savefig(buf, format='png')
-        buf.seek(0)
-        string = base64.b64encode(buf.read())
-        graph = urllib.parse.quote(string)
-        uri = {
-            'image': graph,
-            'derivada': pretty(derivada)
-        }
-        return render(request, 'derivada.html', {'data': uri})
+        print(request.POST)
+        if request.POST.get('inlineRadioOptions') == 'derivada':
+            try:
+                return derivada(data, request)
+            except:
+                return render(request, 'derivada.html', {'erro': True})
+        elif request.POST.get('inlineRadioOptions') == 'integral':
+            try:
+                return integral(data, request)
+            except:
+                return render(request, 'derivada.html', {'erro': True})
+        elif request.POST.get('inlineRadioOptions') == 'limite':
+                return limite(data, request)
+        else:
+            return render(request, 'derivada.html', {'erro': True, 'motivo': 'selecione um método de calculo'})
     return render(request, 'derivada.html')
+
+
+def derivada(data, request):
+    f = Lambda(x, data)
+    derivada = diff(f(x), x)
+    graph = image(f(x))
+    uri = {
+        'expressao': pretty(f.expr),
+        'expressao_escrita': f.expr,
+        'image': graph,
+        'derivada': pretty(derivada),
+        'derivada_escrita': derivada,
+        'erro': False
+    }
+    return render(request, 'derivada.html', {'data': uri})
+
+
+def limite(data, request):
+    ponto = data.split('->')
+    expressao = ponto[1].split('=')
+    lim = Limit(expressao[1].strip(), ponto[0].strip(), expressao[0].strip())
+    lim_img = limit(expressao[1].strip(), ponto[0].strip(), expressao[0].strip())
+    uri = {
+        'expressao': pretty(lim),
+        'expressao_escrita': lim,
+        'image': image(expressao[1].strip()),
+        'derivada_escrita': lim.doit(),
+        'erro': False
+    }
+    return render(request, 'derivada.html', {'data': uri})
+
+
+def integral(data, request):
+    integ = Integral(data, x)
+    integ_img = integrate(data, x)
+    uri = {
+        'expressao': pretty(integ),
+        'expressao_escrita': integ.args,
+        'derivada_escrita': integ.doit(),
+        'image': image(integ_img),
+        'erro': False
+    }
+    return render(request, 'derivada.html', {'data': uri})
+
+
+def image(f):
+    image = plot(f, line_color='green', show=False)
+    buf = io.BytesIO()
+    image.save(buf)
+    buf.seek(0)
+    string = base64.b64encode(buf.read())
+    graph = urllib.parse.quote(string)
+    return graph
